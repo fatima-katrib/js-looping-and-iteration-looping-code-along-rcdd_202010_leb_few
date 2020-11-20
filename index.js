f266635fe418b078a305948a7ef03339bf84bdac
jovial-application-1943 <fatima.katrib@lau.edu>
2020-11-20 21:09:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names,event) {
+  messages=[]
+  for(let i=0; i<names.length; i++){
+    messages.push('Thank you, ${names[i]}, for the wonderful birthday gift!')
+  }
+  return messages
+}
+
+function countDown(n){
+  let i=n;
+  while (i>=0) {
+    console.log('n')
+    n--;
+  }
+}
