c7642d11a11d668dc5a246ba229e4d147138c99d
jovial-application-1943 <fatima.katrib@lau.edu>
2020-11-20 21:12:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 function writeCards(names,event) {
-  messages=[]
+  messages=[];
   for(let i=0; i<names.length; i++){
-    messages.push('Thank you, ${names[i]}, for the wonderful birthday gift!')
+    messages.push("Thank you, "+names[i]+" , for the wonderful birthday gift!");
   }
-  return messages
+  return messages;
 }
 
 function countDown(n){
